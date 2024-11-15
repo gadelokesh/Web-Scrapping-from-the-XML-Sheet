35848c2808fe06d6b419e1d0443ed44253ad5c61
L. <130353133+gadelokesh@users.noreply.github.com>
2024-11-15 08:04:52 +0530

    Add files via upload

---
+++ 1. Extract text.py
@@ -0,0 +1,41 @@
+# Web scrapping for xml sheet 
+
+import os
+os.chdir(r"C:\Users\A3MAX SOFTWARE TECH\Desktop\WORK\2. DATASCIENCE PROJECT\14. NLP WEB SCRAPING\xml_single articles")
+
+import xml.etree.ElementTree as ET
+
+tree = ET.parse("769952.xml") 
+root = tree.getroot()
+
+root=ET.tostring(root, encoding='utf8').decode('utf8')
+
+root 
+
+import re, string, unicodedata
+import nltk
+
+from bs4 import BeautifulSoup
+#from nltk import word_tokenize, sent_tokenize
+#from nltk.corpus import stopwords
+#from nltk.stem import LancasterStemmer, WordNetLemmatizer
+
+def strip_html(text):
+    soup = BeautifulSoup(text, "html.parser")
+    return soup.get_text()
+
+def remove_between_square_brackets(text):
+    return re.sub('\[[^]]*\]', '', text)
+
+def denoise_text(text):
+    text = strip_html(text)
+    text = remove_between_square_brackets(text)
+    text=re.sub('  ','',text)
+    return text
+
+sample = denoise_text(root)
+#print(sample)
+
+
+# https://www.kaggle.com/code/mpwolke/scrapping-deutsche-wikibooks
+#https://www.kaggle.com/code/zackakil/web-scraping-nlp-practical-exercise
